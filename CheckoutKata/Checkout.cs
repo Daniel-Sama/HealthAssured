af1204dad5e94ac57f4935bfb3d40e46a67e0536
Danny Chen <master.danny.chen@gmail.com>
2025-10-07 14:48:25 +0100

    added initial Scan() implemention and PriceRule model

--- CheckoutKata/Checkout.cs
+++ CheckoutKata/Checkout.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CheckoutKata.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,38 @@ using System.Threading.Tasks;
 
 namespace CheckoutKata
 {
-    public sealed class Checkout
+    public sealed class Checkout : ICheckout
     {
+        private readonly List<PricingRule> pricingRules;
+        private readonly Dictionary<string, int> scannedItems = new Dictionary<string, int>();
 
+        public Checkout(List<PricingRule> pricingRules)
+        {
+
+            this.pricingRules = pricingRules;
+
+        }
+
+        public void Scan(string item)
+        {
+
+            if (scannedItems.ContainsKey(item))
+            {
+                scannedItems[item] = scannedItems[item] + 1;
+            }
+            else
+            {
+                scannedItems[item] = 1;
+            }
+
+        }
+
+        public int GetTotalPrice()
+        {
+
+            return 0;
+
+        }
 
 
     }
---
+++ CheckoutKata/PricingRule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKata
+{
+    public class PricingRule
+    {
+
+        public string SKU { get; set; }
+        public int UnitPrice { get; set; }
+        public int? SpecialQuantity { get; set; }
+        public int? SpecialPrice { get; set; }
+
+    }
+}
